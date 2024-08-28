547b614806e253ce989ee6e3ff62247bed594d5e
Regina158 <reginantx5@gmail.com>
2024-08-29 01:03:06 +0700

    menambahkan fitur ligbox didalam gallery dan homepage

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -1,24 +1,35 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import ImageCard from "../components/ImageCard";
 import {
   getGallery,
   getFooter,
   getPengumuman,
   getPengumumanImageUrl,
+  getImageUrlGallery,
 } from "../fetch/api";
-
+import { SlideshowLightbox } from "lightbox.js-react";
+import "lightbox.js-react/dist/index.css";
 export default function GalleryCard() {
   const [gallery, setGallery] = useState([]);
   const [pengumuman, setPengumuman] = useState([]);
   const [footer, setFooter] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  const [lightboxImages, setLightboxImages] = useState([]);
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
   useEffect(() => {
     const fetchGallery = async () => {
       try {
         const dataGallery = await getGallery();
         setGallery(dataGallery.slice(0, 4)); // Ambil 4 album pertama
+        // Prepare lightbox images
+        const lightboxImgs = dataGallery.slice(0, 4).map((item) => ({
+          src: getImageUrlGallery(item.gambar),
+          title: item.judul_album,
+          description: item.ket_album,
+          id: item.id,
+        }));
+        setLightboxImages(lightboxImgs);
       } catch (error) {
         console.log("Failed to fetch Gallery data");
       }
@@ -56,6 +67,9 @@ export default function GalleryCard() {
     fetchFooter();
   }, []);
 
+  const handleToggleView = useCallback(() => {
+    setIsLightboxOpen((prev) => !prev);
+  }, []);
   return (
     <div className="container mx-auto px-4 pt-10 pb-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -65,42 +79,64 @@ export default function GalleryCard() {
             <h1 className="font-bold text-gray-900 dark:text-gray-100 text-2xl">
               Gallery
             </h1>
-            <a
-              href="/gallery"
-              className="inline-flex items-center px-3 py-1.5 font-medium bg-blue-500 hover:bg-blue-600 text-white rounded-full text-xs"
-            >
-              Lihat Semua Gallery
-              <svg
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-3 h-3 ml-1"
+            <div className="flex gap-5">
+              <button
+                className={`rounded-full px-3 py-1.5 font-medium bg-blue-500 hover:bg-blue-600 text-xs ${
+                  isLightboxOpen
+                    ? "bg-gray-200 text-gray-800"
+                    : "bg-blue-500 text-white"
+                }`}
+                onClick={handleToggleView}
               >
-                <path
-                  d="M20 4L12 12M20 4V8.5M20 4H15.5M19 12.5V16.8C19 17.9201 19 18.4802 18.782 18.908C18.5903 19.2843 18.2843 19.5903 17.908 19.782C17.4802 20 16.9201 20 15.8 20H7.2C6.0799 20 5.51984 20 5.09202 19.782C4.71569 19.5903 4.40973 19.2843 4.21799 18.908C4 18.4802 4 17.9201 4 16.8V8.2C4 7.0799 4 6.51984 4.21799 6.09202C4.40973 5.71569 4.71569 5.40973 5.09202 5.21799C5.51984 5 6.07989 5 7.2 5H11.5"
-                  stroke="#ffffff"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                ></path>
-              </svg>
-            </a>
+                {isLightboxOpen ? "View Gallery" : "View Lightbox"}
+              </button>
+              <a
+                href="/gallery"
+                className="inline-flex items-center px-3 py-1.5 font-medium bg-blue-500 hover:bg-blue-600 text-white rounded-full text-xs"
+              >
+                Lihat Semua Gallery
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-3 h-3 ml-1"
+                >
+                  <path
+                    d="M20 4L12 12M20 4V8.5M20 4H15.5M19 12.5V16.8C19 17.9201 19 18.4802 18.782 18.908C18.5903 19.2843 18.2843 19.5903 17.908 19.782C17.4802 20 16.9201 20 15.8 20H7.2C6.0799 20 5.51984 20 5.09202 19.782C4.71569 19.5903 4.40973 19.2843 4.21799 18.908C4 18.4802 4 17.9201 4 16.8V8.2C4 7.0799 4 6.51984 4.21799 6.09202C4.40973 5.71569 4.71569 5.40973 5.09202 5.21799C5.51984 5 6.07989 5 7.2 5H11.5"
+                    stroke="#ffffff"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                  ></path>
+                </svg>
+              </a>
+            </div>
           </div>
-          <div className="mt-4 grid grid-cols-2 gap-4">
-            {gallery.map((item) => (
-              <ImageCard
-                key={item.id}
-                id={item.id}
-                imageUrl={item.gambar}
-                title={item.judul_album}
-                isiPost={item.ket_album}
-                tanggalUpload={item.tanggal_terbit}
-                className="w-full h-full" // Sesuaikan card menjadi w-full dan h-full
-                style={{ maxWidth: "300px", height: "auto" }} // Mengatur proporsi gambar
-              />
-            ))}
-          </div>
+          {isLightboxOpen ? (
+            <SlideshowLightbox
+              images={lightboxImages}
+              showThumbnails={true}
+              open={isLightboxOpen}
+              lightboxIdentifier="lbox1"
+              onClose={handleToggleView}
+              showTitle={true}
+            />
+          ) : (
+            <div className="mt-4 grid grid-cols-2 gap-4">
+              {gallery.map((item) => (
+                <ImageCard
+                  key={item.id}
+                  id={item.id}
+                  imageUrl={item.gambar}
+                  title={item.judul_album}
+                  isiPost={item.ket_album}
+                  tanggalUpload={item.tanggal_terbit}
+                  className="w-full h-full" // Sesuaikan card menjadi w-full dan h-full
+                  style={{ maxWidth: "300px", height: "auto" }} // Mengatur proporsi gambar
+                />
+              ))}
+            </div>
+          )}
         </div>
-
         {/* Pengumuman Section */}
         <div className="bg-gray-100 dark:bg-gray-800 shadow-md p-4">
           <div className="mb-4">
--- src/pages/ListGallery.jsx
+++ src/pages/ListGallery.jsx
@@ -1,24 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import ImageCard from "../components/ImageCard";
-import Pagination from "../components/Pagination"; // Import Pagination
-import { getGallery } from "../fetch/api";
+import Pagination from "../components/Pagination";
+import { getGallery, getImageUrlGallery } from "../fetch/api";
+import { SlideshowLightbox } from "lightbox.js-react";
+import "lightbox.js-react/dist/index.css";
 
 export default function ListGallery() {
   const [gallery, setGallery] = useState([]);
-  const [page, setPage] = useState(1); // State untuk halaman saat ini
+  const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
+  const [lightboxImages, setLightboxImages] = useState([]);
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+  const navigate = useNavigate();
 
-  const itemsPerPage = 8; // Tentukan jumlah item per halaman
+  const itemsPerPage = 8;
+
   useEffect(() => {
-    AOS.init({ duration: 1000 }); // Inisialisasi AOS dengan durasi animasi 1000ms
+    AOS.init({ duration: 1000 });
   }, []);
+
   useEffect(() => {
     const fetchGallery = async () => {
       try {
         const dataGallery = await getGallery();
         setGallery(dataGallery);
+        // Prepare lightbox images
+        const lightboxImgs = dataGallery.map((item) => ({
+          src: getImageUrlGallery(item.gambar),
+          title: item.judul_album,
+          description: item.ket_album,
+          id: item.id,
+        }));
+        setLightboxImages(lightboxImgs);
       } catch (error) {
         console.log("Failed to fetch Gallery data");
       } finally {
@@ -40,37 +56,80 @@ export default function ListGallery() {
     page * itemsPerPage
   );
 
-  if (loading)
+  const handleToggleView = useCallback(() => {
+    setIsLightboxOpen((prev) => !prev);
+  }, []);
+
+  const handleImageClick = useCallback(
+    (image) => {
+      const sanitizedTitle = image.title
+        .replace(/[^a-zA-Z\d\s]/g, "")
+        .split(" ")
+        .join("-")
+        .toLowerCase();
+      navigate(`/gallery_album/${sanitizedTitle}?id=${image.id}`);
+    },
+    [navigate]
+  );
+
+  if (loading) {
     return (
       <div className="flex justify-center items-center min-h-screen">
         <span className="text-lg">Loading...</span>
       </div>
     );
+  }
 
   return (
     <>
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-5"></div>
-      <div data-aos="fade-down">
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8 pb-12">
-          <div className="mt-10 mx-auto grid max-w-2xl grid-cols-2 gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none md:grid-cols-3 lg:grid-cols-4">
-            {GalleryYangDitampilkan.map((item) => (
-              <ImageCard
-                key={item.id}
-                id={item.id}
-                imageUrl={item.gambar}
-                title={item.judul_album}
-                isiPost={item.ket_album}
-                tanggalUpload={item.tanggal_terbit}
-              />
-            ))}
-          </div>
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-28 ">
+        <div className="flex justify-start space-x-4 mb-5">
+          <button
+            className={`rounded-full px-4 py-2 text-sm font-medium bg-blue-500 hover:bg-blue-600 ${
+              isLightboxOpen
+                ? "bg-gray-200 text-gray-800"
+                : " bg-blue-500 text-white"
+            }`}
+            onClick={handleToggleView}
+          >
+            {isLightboxOpen ? "View Gallery" : "View Lightbox"}
+          </button>
         </div>
       </div>
-      <Pagination
-        count={totalPages}
-        page={page}
-        onPageChange={handleChangePage}
+
+      <SlideshowLightbox
+        images={lightboxImages}
+        showThumbnails={true}
+        open={isLightboxOpen}
+        lightboxIdentifier="lbox1"
+        onClose={handleToggleView}
+        showTitle={true}
+        onImageClick={handleImageClick}
       />
+
+      {!isLightboxOpen && (
+        <div data-aos="fade-down">
+          <div className="container mx-auto px-4 sm:px-6 lg:px-8 pb-12">
+            <div className="mt-10 mx-auto grid max-w-2xl grid-cols-2 gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none md:grid-cols-3 lg:grid-cols-4">
+              {GalleryYangDitampilkan.map((item) => (
+                <ImageCard
+                  key={item.id}
+                  id={item.id}
+                  imageUrl={item.gambar}
+                  title={item.judul_album}
+                  isiPost={item.ket_album}
+                  tanggalUpload={item.tanggal_terbit}
+                />
+              ))}
+            </div>
+          </div>
+          <Pagination
+            count={totalPages}
+            page={page}
+            onPageChange={handleChangePage}
+          />
+        </div>
+      )}
     </>
   );
 }
